6ea6436c886657660888c574f75dc2ddd8cef045
Clone888 <dwclone@gmail.com>
2023-09-25 09:43:38 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+string[] listnumbers = File.ReadAllLines("../../../Numbers.txt");
+
+Console.WriteLine("Original order:");
+int temp;
+foreach (string line in listnumbers)
+{
+    Console.Write(line + ",");
+
+    while (int.TryParse(line, out int num))
+    {
+       
+    }
+  
+
+}
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Sorted order:");
+
+
+
+
+foreach (var item in listnumbers)
+{
+    Console.Write(item);
+}
